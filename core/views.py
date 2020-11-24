ffc68269c6acfa35d0e87efb20c8e6a0192d5beb
Welleson Lukas <wellesonlukas@gmail.com>
2020-11-24 10:40:11 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse, JsonResponse
+from .models import Doc
+
+class MainView(TemplateView):
+    template_name = 'core/main.html'
+
+def file_upload_view(request):
+    #print(request.FILES)
+    if request.method == 'POST':
+        my_file = request.FILes.get('file')
+        Doc.objects.create(upload=my_file)
+        return HttpResponse('')
+    return JsonResponse({'post':'false'})
